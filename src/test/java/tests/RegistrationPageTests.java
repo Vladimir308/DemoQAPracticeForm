6f3c17c7e8974c5a18ab20be51c23779f68112d2
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-08-12 22:56:21 +0300

    done

--- src/test/java/tests/RegistrationPageTests.java
+++ src/test/java/tests/RegistrationPageTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-@Tag("regression")
 public class RegistrationPageTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
